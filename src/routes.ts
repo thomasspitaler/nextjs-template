c7748b30d4fc9ac4ad86919134357cb5115e19d0
Thomas Spitaler <ts@easybooking.at>
2018-04-22 12:58:12 +0200

    use route names, add routes for news section

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,11 @@ const createRoutes = require('next-routes')
 const routes = createRoutes()
 
 routes
-    // .add('index', '/') // not needed because files in pages have a public routes per default
-    // .add('about', '/') // not needed because files in pages have a public routes per default 
-    .add('news entry', 'news/entry', 'news/:slug')
+    // .add('<name of route>', '<url pattern>', '<path to page>')
+    .add('index', '/', 'index')
+    .add('about', '/about', 'about')
+    .add('news index', '/news', 'news/index')
+    .add('news entry', '/news/:slug', 'news/entry')
 
 const Link = routes.Link
 const Router = routes.Router
